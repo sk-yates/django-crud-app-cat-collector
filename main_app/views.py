5ea8d02b86eebd838142bb619c66a92fd604599d
sk-yates <syates.edu@gmail.com>
2025-02-05 20:44:56 +0000

    django-models: complete / next:Dj's ORM in Terminal

--- main_app/views.py
+++ main_app/views.py
@@ -3,12 +3,12 @@
 from django.shortcuts import render
 
 # Import HttpResponse to send text-based responses
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Define the home view function
 def home(request):
     # Send a simple HTML response
-    return HttpResponse('<h1> Hello ᓚᘏᗢ </h1>')
+    return render(request, 'home.html')
 
 def about(request):
     # Send a simple HTML response
