29e8c15481d92e744c244a8e814d52a1ab85a523
sk-yates <syates.edu@gmail.com>
2025-02-06 21:00:12 +0000

    Curr: Using Dj's ORM in App / Just created Superuser

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 # main_app/views.py
 
 from django.shortcuts import render
+from .models import Cat
 
 # Import HttpResponse to send text-based responses
 # from django.http import HttpResponse
@@ -15,22 +16,26 @@ def about(request):
     return render(request, 'about.html')
 
 
+# def cat_index(request):
+#     # Render the cats/index.html template with the cats data
+#     return render(request, 'cats/index.html', {'cats': cats})
+
+# class Cat:
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
+
+# # Create a list of Cat instances
+# cats = [
+#     Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+#     Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+#     Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+#     Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+# ]
+
+
 def cat_index(request):
-    # Render the cats/index.html template with the cats data
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
-
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-# Create a list of Cat instances
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
-
