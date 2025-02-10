6d7d12fcfa54a54980bdb9b4e4f5a979043c9fc7
sk-yates <syates.edu@gmail.com>
2025-02-10 21:23:58 +0000

    ORM in app & class based views: complete

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,9 @@
 # main_app/views.py
 
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
+
 
 # Import HttpResponse to send text-based responses
 # from django.http import HttpResponse
@@ -39,3 +41,21 @@ def about(request):
 def cat_index(request):
     cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+    # success_url = '/cats/'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
