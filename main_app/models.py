cd4f14e60ca6315eefe4a8d8974b1a569d271f15
sk-yates <syates.edu@gmail.com>
2025-02-17 20:41:09 +0000

    Completed Auth Lec

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 class Toy(models.Model):
     name = models.CharField(max_length=50)
@@ -18,6 +20,7 @@ class Cat(models.Model):
     description = models.TextField(max_length=250)
     age = models.IntegerField()
     toys = models.ManyToManyField(Toy)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -6,14 +6,48 @@ from django.views.generic import ListView, DetailView
 from .models import Cat, Toy
 from .forms import FeedingForm
 
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+
 
 # Import HttpResponse to send text-based responses
 # from django.http import HttpResponse
 
 # Define the home view function
-def home(request):
-    # Send a simple HTML response
-    return render(request, 'home.html')
+# def home(request):
+#     # Send a simple HTML response
+#     return render(request, 'home.html')
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        # This is how to create a 'user' form object
+        # that includes the data from the browser
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # This will add the user to the database
+            user = form.save()
+            # This is how we log a user in
+            login(request, user)
+            return redirect('cat-index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    # A bad POST or a GET request, so render signup.html with an empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'signup.html', context)
+    # Same as: 
+    # return render(
+    #     request, 
+    #     'signup.html',
+    #     {'form': form, 'error_message': error_message}
+    # )
+
+
+class Home(LoginView):
+    template_name = 'home.html'
 
 def about(request):
     # Send a simple HTML response
@@ -39,11 +73,13 @@ def about(request):
 #     Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
 # ]
 
-
+@login_required
 def cat_index(request):
-    cats = Cat.objects.all()
+    cats = Cat.objects.filter(user=request.user)
+    # cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
 
+@login_required
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
     toys_cat_doesnt_have = Toy.objects.exclude(id__in = cat.toys.all().values_list('id'))
@@ -54,6 +90,11 @@ class CatCreate(CreateView):
     model = Cat
     fields = ['name', 'breed', 'description', 'age']
     # success_url = '/cats/'
+    def form_valid(self, form):
+        # Assign the logged in user (self.request.user)
+        form.instance.user = self.request.user  # form.instance is the cat
+        # Let the CreateView do its job as usual
+        return super().form_valid(form)
 
 class CatUpdate(UpdateView):
     model = Cat
@@ -64,6 +105,7 @@ class CatDelete(DeleteView):
     model = Cat
     success_url = '/cats/'
 
+@login_required
 def add_feeding(request, cat_id):
     # create a ModelForm instance using the data in request.POST
     form = FeedingForm(request.POST)
@@ -94,11 +136,13 @@ class ToyDelete(DeleteView):
     model = Toy
     success_url = '/toys/'
 
+@login_required
 def associate_toy(request, cat_id, toy_id):
     # Note that you can pass a toy's id instead of the whole object
     Cat.objects.get(id=cat_id).toys.add(toy_id)
     return redirect('cat-detail', cat_id=cat_id)
 
+@login_required
 def remove_toy(request, cat_id, toy_id):
     cat, toy = Cat.objects.get(id=cat_id), Toy.objects.get(id=toy_id)
     cat.toys.remove(toy)
